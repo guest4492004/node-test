873468134d10e9ec5dc7e83db4609d4cdc30943a
guest449 <guest4492004@gmail.com>
2021-02-20 00:31:22 +0000

    build again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log("hello");
-  res.write("hi there2.");
+  res.write("hi there on ec2.");
   res.end();
 });
 
